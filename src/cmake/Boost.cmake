1710098464252dcc8372316893f911318b5a4282
Michael Bacarella <michael.bacarella@gmail.com>
2021-08-09 07:45:32 -0700

    Fix boost download link and also replace insecure MD5 with SHA256

--- src/cmake/Boost.cmake
+++ src/cmake/Boost.cmake
@@ -1,8 +1,8 @@
 find_package ( Boost 1.58 )
 
 if (NOT Boost_FOUND )
-	set ( BOOST_URL "https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2")
-	set ( BOOST_MD5 "93eecce2abed9d2442c9676914709349")
+	set ( BOOST_URL "https://boostorg.jfrog.io/artifactory/main/release/1.64.0/source/boost_1_64_0.tar.bz2")
+  set ( BOOST_SHA256 "7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332")
 	set ( BOOST_ZIP_OUT ${CMAKE_CURRENT_BINARY_DIR}/boost_1_64_0.tar.bz2 )
 	set ( BOOST_ROOT ${CMAKE_CURRENT_BINARY_DIR}/boost_1_64_0 )
 
@@ -10,7 +10,7 @@ if (NOT Boost_FOUND )
 		# Download zip file
 		message ("Downloading ${BOOST_URL}")
 		file (DOWNLOAD ${BOOST_URL} ${BOOST_ZIP_OUT}
-			EXPECTED_MD5 ${BOOST_MD5}
+      EXPECTED_SHA256 ${BOOST_SHA256}
 			SHOW_PROGRESS STATUS status)
 		list ( GET status 0 ret )
 		list ( GET status 0 str)
